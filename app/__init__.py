302d2fa8d32cd6b4156d015add6e45e1744b614c
66nihaal44 <nihaalshetty171@gmail.com>
2026-04-15 23:48:20 -0500

    Integrate Flask-Limiter for rate limiting
    Added rate limiting functionality using Flask-Limiter.

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,25 @@ from flask_cors import CORS
 from .engine import init_db
 from .mainroute import main
 import os
+from flask_limiter import Limiter
+
+def get_client_id():
+  forwarded_for = request.headers.get("X-Forwarded-For")
+  if forwarded_for:
+    return forwarded_for.split(",")[0].strip[]
+  return request.remote_addr
+
+limiter = Limiter(
+  key_func=get_client_id,
+  storage_uri=os.getenv("REDIS_URL"),
+  strategy="fixed-window"
+  default_limits=["200 per day", "50 per hour"]
+)
 
 def create_app():
   app = Flask(__name__)
   app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+  limiter.init_app(app)
   init_db(app)
   app.register_blueprint(main)
   CORS(app)
