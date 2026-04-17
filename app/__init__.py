e1f3d73cad2f41e2558334e8c168684a64ccb4f5
66nihaal44 <nihaalshetty171@gmail.com>
2026-04-16 20:43:19 -0500

    Refactor app initialization and import limiter

--- app/__init__.py
+++ app/__init__.py
@@ -2,21 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from .engine import init_db
 from .mainroute import main
+from .limit import limiter
 import os
-from flask_limiter import Limiter
-
-def get_client_id():
-  forwarded_for = request.headers.get("X-Forwarded-For")
-  if forwarded_for:
-    return forwarded_for.split(",")[0].strip()
-  return request.remote_addr
-
-limiter = Limiter(
-  key_func=get_client_id,
-  storage_uri=os.getenv("REDIS_URL"),
-  strategy="fixed-window",
-  default_limits=["200 per day", "50 per hour"]
-)
 
 def create_app():
   app = Flask(__name__)
