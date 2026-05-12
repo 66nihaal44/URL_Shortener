ad21c16adb7912ba8d6ce78b48b403337e20b891
66nihaal44 <nihaalshetty171@gmail.com>
2026-05-12 16:37:00 -0500

    Update utility.py

--- app/utility.py
+++ app/utility.py
@@ -5,3 +5,11 @@ def is_valid_url(url):
   return (parsed.scheme in ("http", "https") and 
          parsed.netloc and
          "." in parsed.netloc)
+
+# Helper for redirect_handler
+def password_entry(url): 
+  if url.hashed_password:
+    sub_password = request.form.get("password")
+    if not sub_password or not check_password_hash(url.hashed_password, sub_password):
+      return render_template("password_prompt.html"), 401
+  return
