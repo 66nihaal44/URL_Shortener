fc6f01cf8f1e9edbd964e9d45db5e918064970e3
66nihaal44 <nihaalshetty171@gmail.com>
2026-05-13 21:03:24 -0500

    Update utility.py

--- app/utility.py
+++ app/utility.py
@@ -7,9 +7,7 @@ def is_valid_url(url):
          "." in parsed.netloc)
 
 # Helper for redirect_handler
-def password_entry(password): 
-  if password:
-    sub_password = request.form.get("password")
-    if not sub_password or not check_password_hash(password, sub_password):
-      return render_template("password_prompt.html"), 401
-  return
+def password_entry(password):
+  sub_password = request.form.get("password")
+  if not sub_password or not check_password_hash(password, sub_password):
+    return render_template("password_prompt.html"), 401
