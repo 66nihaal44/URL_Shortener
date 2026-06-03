51fbfa2b62822f8221c98b077884e6fad514fb3d
66nihaal44 <nihaalshetty171@gmail.com>
2026-06-03 01:41:55 -0500

    Update mainroute.py

--- app/mainroute.py
+++ app/mainroute.py
@@ -114,7 +114,9 @@ def clicks_referrers():
     results = ( session.query(Click.referrer, func.count(Click.id))
                 .group_by(Click.referrer).all()
               )
-    results = [tuple(row) for row in results]
+  #for row, index in results
+  #results = {row[0]: row[1] for row in results} # create dictionary instead of tuples
+  results = [tuple(row) for row in results]
   finally:
     session.close()
   return {"clicks_referrers": results}
